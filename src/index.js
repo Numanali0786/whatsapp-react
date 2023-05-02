0ea164a52d152fe32203529e327f798eb1114a49
Numan Ali <numan4599322ali@gmail.com>
2023-05-02 14:38:06 +0530

    first

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
@@ -8,9 +8,11 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <GoogleOAuthProvider clientId="231723265771-5revqoolgkrdn7tsepi6u98dq0qtdr0d.apps.googleusercontent.com">
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </GoogleOAuthProvider>
+  <StrictMode>
+    <GoogleOAuthProvider clientId="231723265771-5revqoolgkrdn7tsepi6u98dq0qtdr0d.apps.googleusercontent.com">
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </GoogleOAuthProvider>
+  </StrictMode>
 );
